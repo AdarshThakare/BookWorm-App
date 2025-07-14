234230684a105c8d928149d0e9314e34621db58e
Adarsh Thakare <adarshythakare@gmail.com>
2025-07-14 09:23:35 +0530

    backend cron job resolve

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,8 +10,7 @@ import job from "./lib/cron.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-job.start();
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
